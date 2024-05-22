bcde986bf0b0842bbac73957e555e8567993075f
Allan Viray <allang.v.dev@gmail.com>
2024-05-22 14:02:24 -0500

    Move files related logic into FileList

--- src/components/FileList/FileList.jsx
+++ src/components/FileList/FileList.jsx
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react';
 import FileItem from '../FileItem/FileItem';
+import { fetchFiles } from '../../utilities/files-api';
 
-export default function FileList({ files }) {
+export default function FileList() {
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    async function getFiles() {
+      setFiles(await fetchFiles());
+    }
+    getFiles();
+  }, []);
+
   const fileListItems = files.map((file, idx) => <FileItem file={file} key={idx} />);
+
   return (
     <>
+    {
+      files.length === 0 ? <h3>No Files Yet!</h3>
+        : <>
       <h1>Files</h1>
       <ul>{fileListItems}</ul>
+      </>
+    }
     </>
   );
 }
